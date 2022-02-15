6d999e677cc745376228d9656578ef90f42b0d83
Lee Siang Yin <siangyin916@gmail.com>
2022-02-16 02:33:44 +0800

    setup commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -16,6 +18,9 @@ app.use(morgan("common"));
 app.get("/", (req, res) => {
 	res.send("welcome");
 });
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
 	mongoose.connect(
